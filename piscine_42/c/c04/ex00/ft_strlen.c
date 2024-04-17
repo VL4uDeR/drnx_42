cc19f2bf1b387065e402b7118530d6a23030c60b
Darin Salgado <darinlevi91@gmail.com>
2024-04-17 02:00:28 +0200

    added c04/ex00/

---
+++ piscine_42/c/c04/ex00/ft_strlen.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+	
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	main(void)
+{
+	char *saludo;
+
+	saludo = "holaMundo!";
+	printf("%d\n", ft_strlen(saludo));
+	return (0);
+}
