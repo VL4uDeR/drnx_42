cd3e5b36dd2a91efa5f787e1f6570128e3c4848d
Darin Salgado <darinlevi91@gmail.com>
2024-04-26 02:47:14 +0200

    add (42header & norminnete) modded c04/ - ex00/ ex01/ ex02/

--- piscine_42/c/c04/ex00/ft_strlen.c
+++ piscine_42/c/c04/ex00/ft_strlen.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: darsalga <darsalga@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/26 02:30:11 by darsalga          #+#    #+#             */
+/*   Updated: 2024/04/26 02:31:43 by darsalga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <stdio.h>
 
 int	ft_strlen(char *str)
 {
 	int	i;
-	
+
 	i = 0;
 	while (str[i] != 0)
 		i++;
@@ -13,7 +25,7 @@ int	ft_strlen(char *str)
 
 int	main(void)
 {
-	char *saludo;
+	char	*saludo;
 
 	saludo = "holaMundo!";
 	printf("%d\n", ft_strlen(saludo));
