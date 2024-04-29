2fe914dac701c3481de79a56563eab0cb1c39532
Darin Salgado <darinlevi91@gmail.com>
2024-04-29 03:08:48 +0200

    modded ft_putnbr.c - added ft_atoi.c

--- piscine_42/c/c04/ex03/ft_atoi.c
+++ piscine_42/c/c04/ex03/ft_atoi.c
@@ -1,17 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: darsalga <darsalga@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/26 02:33:04 by darsalga          #+#    #+#             */
-/*   Updated: 2024/04/29 03:06:03 by darsalga         ###   ########.fr       */
+/*   Created: 2024/04/27 19:06:39 by darsalga          #+#    #+#             */
+/*   Updated: 2024/04/29 02:51:20 by darsalga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdlib.h>
 
 void	ft_putchar(char c)
 {
@@ -20,9 +19,9 @@ void	ft_putchar(char c)
 
 void	ft_putnbr(int nb)
 {
-	char	n;
+	char	number;
 
-	n = 0;
+	number = 0;
 	if (nb < 0)
 	{
 		ft_putchar('-');
@@ -30,14 +29,46 @@ void	ft_putnbr(int nb)
 	}
 	if (nb > 9)
 		ft_putnbr(nb / 10);
-	n = nb % 10 + '0';
-	write(1, &n, 1);
+	number = nb % 10 + '0';
+	ft_putchar(number);
+}
+
+int	ft_atoi(char *str)
+{
+	int	minuscount;
+	int	nbr;
+
+	minuscount = 0;
+	nbr = 0;
+	while (*str == ' ' || *str == '\t' || *str == '\r' || *str == '\n'
+		|| *str == '\v' || *str == '\f')
+		str++;
+	while (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			minuscount++;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		nbr = nbr * 10 + (*str - '0');
+		str++;
+	}
+	if (minuscount % 2 == 0)
+		return (nbr);
+	else
+		return (-nbr);
 }
 
 int	main(int argc, char **argv)
 {
+	char	*saludo;
+
+	saludo = "  --+--+-1234567ab567";
+	if (argc == 1)
+		ft_putnbr(ft_atoi(saludo));
 	if (argc == 2)
-		ft_putnbr(atoi(argv[1]));
+		ft_putnbr(ft_atoi(argv[1]));
 	write(1, "\n", 1);
 	return (0);
 }
