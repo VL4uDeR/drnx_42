7d4f35029378c550a928b694cb9a44a701c2a7fe
Darin Levi Salgado Gajardo <darsalga@cbr5s6.42barcelona.com>
2024-06-21 00:04:07 +0200

    added ft_strrchr.c modded ft_strchr.c

--- common_core/libft/srcs/ft_strrchr.c
+++ common_core/libft/srcs/ft_strrchr.c
@@ -1,43 +1,56 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: darsalga <darsalga@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/19 17:03:18 by darsalga          #+#    #+#             */
-/*   Updated: 2024/06/21 00:00:28 by darsalga         ###   ########.fr       */
+/*   Created: 2024/06/20 20:35:29 by darsalga          #+#    #+#             */
+/*   Updated: 2024/06/21 00:01:45 by darsalga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
 
-char	*ft_strchr(const char *str, int c)
+char	*ft_strrchr(const char *str, int c)
 {
 	int	i;
+	int	count1;
+	int	count2;
 
 	i = 0;
+	count1 = 0;
+	count2 = 0;
 	while (str[i])
 	{
 		if (str[i] == (char)c)
-			return ((char *)&str[i]);
+			count1++;
 		i++;
 	}
-	if (str[i] == (char)c)
+	while (str[i])
+	{
+		if (str[i] == (char)c)
+		{
+			count2++;
+			if (count1 == count2)
+				return ((char *)&str[i]);
+		}
+		i++;
+	}
+	if (str[i] && ((char)c == '\0'))
 		return ((char *)&str[i]);
 	return (NULL);
 }
-/*
+
 int	main(void)
 {
 	char	*sld;
 	int		c;
 
-	sld = "42 es la Respuesta!";
+	sld = "42 es la ResRpuesta!";
 	c = 'R';
-	printf("%s\n", ft_strchr(sld, c));
-	printf("%s\n", strchr(sld, c));
+	printf("%s\n", ft_strrchr(sld, c));
+	printf("%s\n", strrchr(sld, c));
 	return (0);
 }
-*/
