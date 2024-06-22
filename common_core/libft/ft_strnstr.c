fc907b1de874de269ec43a1f7c18e98131e4bf6a
Darin Salgado <darinlevi91@gmail.com>
2024-06-22 04:09:50 +0200

    modded  fn_strnstr.c

--- common_core/libft/ft_strnstr.c
+++ common_core/libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: darsalga <darsalga@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/22 00:27:21 by darsalga          #+#    #+#             */
-/*   Updated: 2024/06/22 02:00:47 by darsalga         ###   ########.fr       */
+/*   Updated: 2024/06/22 04:08:21 by darsalga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	x;
 
 	i = 0;
-	if (little[i] == 0)
+	if (little[i] == '\0')
 		return ((char *)big);
 	while (big[i] && i < len)
 	{
@@ -33,14 +33,15 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	}
 	return (NULL);
 }
-
+/*
 int	main(void)
 {
 	char	*s1 = "holaMundo";
-	char	*s2 = "Mu";
+	char	*s2 = "Mund";
 	size_t	max = 6;
 	char	*i1 = ft_strnstr(s1, s2, max);
 
 	printf("strnstr: %s\n", i1);
 	return (0);
 }
+*/
