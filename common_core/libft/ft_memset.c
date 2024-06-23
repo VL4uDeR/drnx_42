5783a9f00719d087544d8612e859d77fd6d0b97a
Darin Salgado <165274742+VL4uDeR@users.noreply.github.com>
2024-06-23 21:56:25 +0200

    Updated ft_memset.c

--- common_core/libft/ft_memset.c
+++ common_core/libft/ft_memset.c
@@ -2,5 +2,8 @@ void	*ft_memset(void *b, int c, size_t len)
 {
 	unsigned char	*tmp;
 
-	tmp = (unsigned char)c;
+	tmp = (unsigned char *)b;
+	while (len--)
+		*tmp++ = (unsigned char)c;
+	return (b);
 }
