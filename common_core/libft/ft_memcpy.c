60fbd4028a163efd67377878f9ec81b4a1b1a7c5
Darin Salgado <165274742+VL4uDeR@users.noreply.github.com>
2024-06-24 11:12:23 +0000

    modded ft_memcpy.c // testing replit

--- common_core/libft/ft_memcpy.c
+++ common_core/libft/ft_memcpy.c
@@ -1,4 +1,27 @@
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
+	unsigned char *pdst;
+	unsigned char *psrc;
 	
+	if (dst == NULL && src == NULL)
+		return (dst);
+	pdst = (unsigned char *)dst;
+	psrc = (unsigned char *)src;
+	while (n--)
+		*(pdst++) = *(psrc++);
+	return (dst);
+}
+
+int	main(void)
+{
+	char *sld;
+	char copia[20];
+
+	sld = 'holaMundo!!!';
+	copia = 'adeeeu!'
+	ft_memcpy(copia, sld, ft_strlen(sld) + 1);
+	printf("%s\n", copia);
+	return (0);
 }
