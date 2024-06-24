87a920a9b92b1661c8d4efe7c6fdf4dbca30508d
Darin Levi Salgado Gajardo <darsalga@cbr5s6.42barcelona.com>
2024-06-24 16:29:39 +0200

    modded ft_memset.c

--- common_core/libft/ft_memset.c
+++ common_core/libft/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: darsalga <darsalga@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 00:53:08 by darsalga          #+#    #+#             */
-/*   Updated: 2024/06/24 13:43:14 by darsalga         ###   ########.fr       */
+/*   Updated: 2024/06/24 16:17:58 by darsalga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,13 @@ void	*ft_memset(void *b, int c, size_t len)
 
 int	main(void)
 {
-	char buffer[20];
+	char	buffer[20];
 
 	ft_memset(buffer, '\0', sizeof(buffer));
 	printf("buffer: %s\n", buffer);
-	ft_memset(buffer, 'F', sizeof(buffer) - 1)
+	ft_memset(buffer, '2', sizeof(buffer) - 1);
+	printf("buffer: %s\n", buffer);
+	ft_memset(buffer, '4', sizeof(buffer) - sizeof(buffer) + 1);
 	printf("buffer: %s\n", buffer);
 	return (0);
 }
