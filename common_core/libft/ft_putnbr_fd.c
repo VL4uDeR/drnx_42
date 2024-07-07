2a8c77b44f94f1dce59eb0b480b066d9b12bc72e
Darin Levi Salgado Gajardo <darsalga@cbr5s6.42barcelona.com>
2024-07-07 23:56:06 +0200

    renamed ft_lst* files && modded makefile ft_putnbr_fd.c

--- common_core/libft/ft_putnbr_fd.c
+++ common_core/libft/ft_putnbr_fd.c
@@ -6,30 +6,38 @@
 /*   By: darsalga <darsalga@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 20:54:27 by darsalga          #+#    #+#             */
-/*   Updated: 2024/07/03 21:08:53 by darsalga         ###   ########.fr       */
+/*   Updated: 2024/07/07 15:28:15 by darsalga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <unistd.h>
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*nb;
-
-	nb = ft_itoa(n);
-	while (*nb)
+	if (n < 0)
 	{
-		write(fd, &*nb, 1);
-		nb++;
+		if (n == -2147483648)
+		{
+			ft_putstr_fd("-2147483648", fd);
+			return ;
+		}
+		ft_putchar_fd('-', fd);
+		n = -n;
 	}
+	if (n / 10)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	else
+		ft_putchar_fd(n + '0', fd);
 }
 /*
 int	main(void)
 {
 	int	n;
 
-	n = -2147483647;
+	n = -2147483648;
 	ft_putnbr_fd(n, 1);
 	return (0);
 }
