5397f1681b2066da7b4db486b7e630ff83cd808f
Darin Salgado <darinlevi91@gmail.com>
2024-07-21 15:22:10 +0200

    updated files

--- common_core/get_next_line/get_next_line.h
+++ common_core/get_next_line/get_next_line.h
@@ -6,14 +6,16 @@
 /*   By: darsalga <darsalga@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 03:00:52 by darsalga          #+#    #+#             */
-/*   Updated: 2024/07/21 05:30:01 by darsalga         ###   ########.fr       */
+/*   Updated: 2024/07/21 15:21:34 by darsalga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFFER_SIZE 1024
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
 
 # include <stdlib.h>
 # include <stddef.h>
