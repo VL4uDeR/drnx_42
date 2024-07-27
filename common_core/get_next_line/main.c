4abb7738a2fa62052c58d63a555aea9d53b1a543
Darin Salgado <darinlevi91@gmail.com>
2024-07-27 23:15:05 +0200

    added main.c

---
+++ common_core/get_next_line/main.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: darsalga <darsalga@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/24 16:48:02 by darsalga          #+#    #+#             */
+/*   Updated: 2024/07/27 21:03:13 by darsalga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int	main(void)
+{
+	char	*line;
+	int		fd;
+
+	fd = open("storytest.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		perror("Error opening file");
+		return (1);
+	}
+	line = get_next_line(fd);
+	while (line != NULL)
+	{
+		printf("%s", line);
+		free(line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+	printf("Presiona Enter para salir...\n");
+	getchar();
+	return (0);
+}
