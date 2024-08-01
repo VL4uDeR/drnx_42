734b589eeba69301287dc15b28df1b86a8b591d5
Darin Salgado <darinlevi91@gmail.com>
2024-08-01 03:11:16 +0200

    added ft_printf files

---
+++ common_core/printf/ft_printf_utils.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: darsalga <darsalga@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/01 01:07:38 by darsalga          #+#    #+#             */
+/*   Updated: 2024/08/01 03:09:12 by darsalga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include <unistd.h>
+
+int	ft_putchar(int c)
+{
+	return (write(1, &c, 1));
+}
+
+int	ft_putstr(char *s)
+{
+	int	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i])
+	{
+		ft_putchar(s[i]);
+		i++;
+	}
+	return (i);
+}
