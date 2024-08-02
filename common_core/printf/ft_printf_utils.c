e216eed1b10b0622ded1e3aa90b5bcd207258bd4
Darin Salgado <darinlevi91@gmail.com>
2024-08-02 14:07:44 +0200

    modded ft_printf file

--- common_core/printf/ft_printf_utils.c
+++ common_core/printf/ft_printf_utils.c
@@ -6,7 +6,7 @@
 /*   By: darsalga <darsalga@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 01:07:38 by darsalga          #+#    #+#             */
-/*   Updated: 2024/08/01 03:09:12 by darsalga         ###   ########.fr       */
+/*   Updated: 2024/08/02 02:04:30 by darsalga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,4 +31,41 @@ int	ft_putstr(char *s)
 		i++;
 	}
 	return (i);
+}
+
+int	ft_putnbr(int nb)
+{
+	int	n;
+
+	n = 0;
+	if (nb == -2147483648)
+		return (ft_putstr("-2147483648"));
+	if (nb < 0)
+	{
+		n += ft_putchar('-');
+		nb *= -1;
+	}
+	if (nb / 10)
+	{
+		n += ft_putnbr(nb / 10, fd);
+		n += ft_putnbr(nb % 10, fd);
+	}
+	else
+		n += ft_putchar_fd(n + '0', fd);
+	return (n);
+}
+
+int ft_putunbr(unsigned int nb)
+{
+ unsigned int n;
+
+ n = 0;
+ if (nb > 9)
+ {
+    n += ft_putunbr(nb / 10);
+    n += ft_putunbr(nb % 10);
+ }
+ else
+    n += ft_putchar(nb + '0');
+ return (n);
 }
