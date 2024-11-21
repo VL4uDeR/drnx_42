547101e47d737d6909194b720558af0f2d3c6bdd
Darin Levi Salgado Gajardo <darsalga@cbr5s6.42barcelona.com>
2024-11-21 09:06:23 +0100

    created & added exams + putstr

---
+++ exams/exam_rank2/ft_putstr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: darsalga <darsalga@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/21 08:36:28 by darsalga          #+#    #+#             */
+/*   Updated: 2024/11/21 09:01:32 by darsalga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		write(1, &str[i++], 1);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+		ft_putstr(argv[1]);
+	write(1, "\n", 1);
+	return (0);
+}
